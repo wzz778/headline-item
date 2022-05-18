b58737e85cfd9399faa75a48058deabbc2e3c83f
yangsaiya <2728771838@qq.com>
2022-05-18 15:28:08 +0800

    ysy 动态sql语句 2.0

--- src/main/java/com/skynews/dao/PostsMapper.java
+++ src/main/java/com/skynews/dao/PostsMapper.java
@@ -96,7 +96,7 @@ public interface PostsMapper {
     List<Posts> findUser();
 
     //获取用户list 用@Param 对mapper文件 进行多个参数的传递
-    List<Posts> getlist(@Param("start") int start, @Param("count") int count);
+    List<Posts> getList(@Param("start") int start, @Param("count") int count);
 
     //获取用户总数
     Integer getTotal();
