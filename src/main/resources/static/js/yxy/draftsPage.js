f6bbb292824b4fb73120d8816600a15598cbb859
Administrator <3190493163@qq.com>
2022-05-19 15:14:33 +0800

    yxy写好了草稿箱的分页及批量删除

--- src/main/resources/static/js/yxy/draftsPage.js
+++ src/main/resources/static/js/yxy/draftsPage.js
@@ -40,27 +40,31 @@ function sureFade(){
 //页数
 //总页数
 var empty=document.querySelector('.empty');
-$.ajax({
-    type:'post',
-    url:'http://localhost:8080/ToSkyNews_war_exploded/user/drafts',
-    data:{
-        reside:user_id
-    },
-    success:function(res){
-        console.log(res.data.length);
-        if(res.data.length%6==0){
-            allPage.innerHTML=res.data.length/6; 
-        }else if(res.data.length==0){
-            list.style.display='none';
-            empty.style.display='flex';
-        }else if(res.data.length%6!=0){
-            allPage.innerHTML=Math.ceil(res.data.length/6);
+num();
+function num(){
+    $.ajax({
+        type:'post',
+        url:'http://localhost:8080/ToSkyNews_war_exploded/user/drafts',
+        data:{
+            reside:user_id
+        },
+        success:function(res){
+            console.log(res.data.length);
+            if(res.data.length%6==0){
+                allPage.innerHTML=res.data.length/6; 
+            }else if(res.data.length==0){
+                list.style.display='none';
+                empty.style.display='flex';
+            }else if(res.data.length%6!=0){
+                allPage.innerHTML=Math.ceil(res.data.length/6);
+            }
+        },
+        error:function(err){
+            console.log(err);
         }
-    },
-    error:function(err){
-        console.log(err);
-    }
-})
+    })
+}
+
 var pageNum=document.querySelector('.pageNum');
 var lastPage=document.querySelector('.lastPage');
 var nextPage=document.querySelector('.nextPage');
@@ -115,7 +119,7 @@ function draftsContent(num){
             for(let i=0;i<res.data.length;i++){
                 var li=`
                     <li class="draftItem" textID="">
-                        <input type="checkbox" class="chooseBox" checked=false/>
+                        <input type="checkbox" class="chooseBox">
                         <div class="itemContent">
                             <div class="title">${res.data[i].postsName}</div>
                             <div class="lastTime">上次修改的时间：<span class="time">2${res.data[i].picture}</span></div>
@@ -130,6 +134,31 @@ function draftsContent(num){
                 var itemDelete=document.querySelectorAll('.itemDelete');
                 draftItem[i].textID=res.data[i].postsID;
                 // console.log(draftItem[i].textID);
+                //获取页数
+                num();
+                function num(){
+                    $.ajax({
+                        type:'post',
+                        url:'http://localhost:8080/ToSkyNews_war_exploded/user/drafts',
+                        data:{
+                            reside:user_id
+                        },
+                        success:function(res){
+                            console.log(res.data.length);
+                            if(res.data.length%6==0){
+                                allPage.innerHTML=res.data.length/6; 
+                            }else if(res.data.length==0){
+                                list.style.display='none';
+                                empty.style.display='flex';
+                            }else if(res.data.length%6!=0){
+                                allPage.innerHTML=Math.ceil(res.data.length/6);
+                            }
+                        },
+                        error:function(err){
+                            console.log(err);
+                        }
+                    })
+                }
                 //操作
                 var chooseBox=document.querySelectorAll('.chooseBox');
                 massDelete.addEventListener('click',function(){
@@ -140,18 +169,17 @@ function draftsContent(num){
                     cancelAll.style.display='inline-block';
                     reset.style.display='inline-block';
                     for(let i=0;i<chooseBox.length;i++){
-                        chooseBox[i].checked=false;
                         chooseBox[i].style.display='inline-block';
                     }
                 })
                 allChoose.addEventListener('click',function(){
                     for(let i=0;i<chooseBox.length;i++){
-                        chooseBox[i].checked=true;
+                        chooseBox[i].setAttribute('checked',true);
                     }
                 })
                 cancelAll.addEventListener('click',function(){
                     for(let i=0;i<chooseBox.length;i++){
-                        chooseBox[i].checked=false;
+                        chooseBox[i].removeAttribute('checked');
                     }
                 })
                 reset.addEventListener('click',function(){
@@ -162,11 +190,19 @@ function draftsContent(num){
                     massDelete.style.display='block';
                     itemDelete[i].style.display='inline-block';
                     for(let i=0;i<chooseBox.length;i++){
-                        chooseBox[i].checked=false;
+                        chooseBox[i].removeAttribute('checked');
                         chooseBox[i].style.display='none';
                     }
                 })
-                
+                chooseBox[i].addEventListener('click',function(){
+                    if(chooseBox[i].hasAttribute('checked')==true){
+                        chooseBox[i].removeAttribute('checked');
+                    }else{
+                        chooseBox[i].setAttribute('checked',true);
+                    }
+                    
+                })
+               
                 //删除草稿
                 itemDelete[i].addEventListener('click',function(e){
                     e.stopPropagation();
@@ -184,12 +220,17 @@ function draftsContent(num){
                             },
                             success:function(suc){
                                 console.log(suc);
+                                var pageNum=document.querySelector('.pageNum');
+                                num();
+                                if(allPage.innerHTML*1>=pageNum.innerHTML*1){
+                                    draftsContent((pageNum.innerHTML*1-1)*6);
+                                }else{
+                                    draftsContent((allPage.innerHTML*1-1)*6);
+                                }
                                 sureAppear("删除成功！");
                                 successOk.addEventListener('click',function(){
                                     sureFade();
                                 })
-                                var pageNum=document.querySelector('.pageNum');
-                                draftsContent((pageNum.innerHTML*1-1)*6);
                                 list.removeChild(draftItem[i]);
                             },
                             error:function(err){
@@ -214,22 +255,97 @@ function draftsContent(num){
     })    
 }
 
-//批量删除草稿
-deleteChoose.addEventListener('click',function(){
-    var arr=new Array();
-    for(let i=0;i<res.data.length;i++){
-        if(chooseBox[i].checked==true){
-            arr.push(draftItem[i].textID);
-        }else{
-            console.log('我没被选择');
-        }
-    }
-    if(arr.length==0){
-        sureAppear('请选择要删除的草稿');
-        successOk.addEventListener('click',function(){
-            sureFade();
+ //批量删除草稿
+window.onload=function(){
+    // function onclickFun(){
+        deleteChoose.addEventListener('click',function(){
+                var arr=new Array();
+                var  draftItem=document.querySelectorAll('.draftItem') ;
+                var chooseBox=document.querySelectorAll('.chooseBox');
+                for(let j=0;j<draftItem.length;j++){
+                    console.log(chooseBox[j].hasAttribute('checked'));
+                    if(chooseBox[j].hasAttribute('checked')==true){
+                        arr.push(draftItem[j].textID);
+                        // console.log('我被选择')
+                    }
+                    if(arr[j]==undefined){
+                        arr.slice(j);
+                    }
+                    
+                }
+            
+                sureFade();
+                if(arr.length==0){
+                    sureAppear('请选择要删除的草稿');
+                    successOk.addEventListener('click',function(){
+                        sureFade();
+                    })
+                }else{
+                    chooseAppear("确定要删除吗？");
+                    tipNo.addEventListener('click',function(){
+                        chooseAppear();
+                    })
+                    tipOk.addEventListener('click',function(){
+                        chooseFade();
+                        let str='?List<Integer>list=';
+                        for(let i=0;i<arr.length;i++){
+                            console.log(arr[i]);
+                            if(i==arr.length-1){
+                                str+=arr[i];
+                            }else{
+                                str+=arr[i]+'&List<Integer>list=';
+                            }
+                            console.log(str);
+                            $.ajax({
+                                type:'post',
+                                url:'http://localhost:8080/ToSkyNews_war_exploded/collections/deleteBatchPosts'+str,
+                                // data:{
+                                // },
+                                success:function(res_1){
+                                    console.log(res_1);
+                                    sureAppear("删除成功！");
+                                    successOk.addEventListener('click',function(){
+                                        sureFade();
+                                    })
+                                    var pageNum=document.querySelector('.pageNum');
+                                    num();
+                                    if(allPage.innerHTML*1>=pageNum.innerHTML*1){
+                                        draftsContent((pageNum.innerHTML*1-1)*6);
+                                    }else{
+                                        draftsContent((allPage.innerHTML*1-1)*6);
+                                    }
+                                    
+                                },
+                                error:function(err){
+                                    console.log(err);
+                
+                                }
+                            })
+                            
+                        }
+                    })
+                    
+                } 
+            
         })
-    }else{
-        console.log(arr[i]); 
-    }
-})
+       //搜索页数
+       var searchPage=document.querySelector('.searchPage');
+       var sure=document.querySelector('.sure');
+       sure.addEventListener('click',function(){
+           if(searchPage.value==''){
+               sureAppear('请输入要查询的页数');
+               successOk.addEventListener('click',function(){
+                   sureFade();
+               })
+           }else if(searchPage.value*1>allPage.innerHTML*1||searchPage.value*1<=0){
+               sureAppear('请输入有效页数');
+               successOk.addEventListener('click',function(){
+                   sureFade();
+               })
+           }else{
+               list.innerHTML='';
+               pageNum.innerHTML=searchPage.value;
+               draftsContent((pageNum.innerHTML*1-1)*6);
+           }
+       })
+}
